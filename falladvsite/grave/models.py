bfd0387834782812505730be0746fc20a617d8be
Cody <hellocodystuck@gmail.com>
2020-05-25 17:28:02 -0700

    progress

--- falladvsite/grave/models.py
+++ falladvsite/grave/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-# This represents a real person who owns characters
-class Player(models.Model):
-    username = 
-
 # This represents the grave of a player's character
 class Grave(models.Model):
     name = models.CharField(max_length = 50)
