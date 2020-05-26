c5da7130c62aec8db5f2cf2237b37752a54ac84f
Cody <hellocodystuck@gmail.com>
2020-05-26 14:05:42 -0700

    added model, added admin

--- falladvsite/grave/models.py
+++ falladvsite/grave/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # This represents the grave of a player's character
-class Grave(models.Model):
+class Gravemarker(models.Model):
     name = models.CharField(max_length = 50)
     race = models.CharField(max_length = 50)
     charclass = models.CharField(max_length = 50)
     liferange = models.CharField(max_length = 50)
     enscription = models.CharField(max_length = 150)
-    #playedby = models.
+    date_created = models.DateTimeField(default=timezone.now)
+    createdby = models.ForeignKey(User, models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- falladvsite/grave/views.py
+++ falladvsite/grave/views.py
@@ -1,24 +1,6 @@
 from django.shortcuts import render
+from .models import Gravemarker
 #from django.http import HttpResponse
-
-gravestonestest = [
-    {
-        'adventurer_name': 'Kaine',
-        'race': 'Human',
-        'class': 'Wizard',
-        'enscript': 'Here lies Kaine, who bravely opposed the evils of Ravenloft',
-        'date_posted': 'September 19, 2020',
-        'player': 'Adam'
-    },
-    {
-        'adventurer_name': 'Rondall',
-        'race': 'Half-Elf',
-        'class': 'Bard',
-        'enscript': 'This stone remembers Rondall, whose body still lies under the windmill by which he was crushed.',
-        'date_posted': 'September 23, 2020',
-        'player': 'Benjamin'
-    }
-]
 
 # Create your views here.
 def home(request):
@@ -26,7 +8,7 @@ def home(request):
 
 def explore(request):
     context = {
-        'gravestones': gravestonestest
+        'gravestones': Gravemarker.objects.all()
     }
     return render(request, 'grave/explore.html', context)
 
